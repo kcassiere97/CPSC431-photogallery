4df4d376bea02063e5a978d4f014fe856b4937b7
kizar <kcassiere97@csu.fullerton.edu>
2021-02-08 22:31:02 -0800

    write data to file

--- gallery.php
+++ gallery.php
@@ -1,3 +1,12 @@
+<!-- Store data from text-box to a file -->
 <?php
-echo "Hello World!";
+ $path = 'data.txt';
+ if (isset($_POST['photoname_']) && isset($_POST['date_']) && isset($_POST['photographer_']) && isset($_POST['location_'])) {
+    $fh = fopen($path,"a+");
+    $string = $_POST['photoname_'].' - '.$_POST['data_'].' - '.$_POST['photographer_'].' - '.$_POST['location_'];
+    fwrite($fh,$string); // Write information to the file
+    fclose($fh); // Close the file
+ }
 ?>
+
+<!-- Sort strings from the file and output them to html -->
