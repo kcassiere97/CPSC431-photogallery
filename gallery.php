b7acf9ef79b49a08bbd2fc783a4b599c8b5d26c3
kizar <kcassiere97@csu.fullerton.edu>
2021-02-15 15:00:15 -0800

    temp

--- gallery.php
+++ gallery.php
@@ -1,3 +1,9 @@
+<?php
+  // create short variable name
+  $document_root = $_SERVER['DOCUMENT_ROOT'];
+  $file_name = $_FILES["fileToUpload"]["name"];
+?>
+
 <!-- Store data from text-box to a file -->
 <?php
  $path = 'data.txt';
@@ -10,3 +16,16 @@
 ?>
 
 <!-- Sort strings from the file and output them to html -->
+
+<?php
+$path = "data.txt";
+$photoname_ = trim($_POST['photoname_']);
+$email = trim($_POST['date_']);
+$name = trim($_POST['photographer_']);
+$location = trim($_POST['location_']);)
+
+function 
+
+
+
+?> 
