12f4435edf0aeb444cd7e58b22a9cb553b49d21c
Donavie Ordonez <donavielove@csu.fullerton.edu>
2021-02-17 17:17:56 -0800

    Added Drop Down for sorting images in gallery.php

--- gallery.php
+++ gallery.php
@@ -1,7 +1,36 @@
+<html>
+    <meta charset="utf-8">
+    <head>
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="color.css">
+
+    </head>
+  
+        <body class="container">
+                <div class="center">
+                    <h1 class ="p1">View All Photos</h1>
+                    <form action="gallery.php">
+                    <label for="sorting" class ="p1">Sort By:</label>
+                    <select name="sorting" id="sorting">
+                      <option value="name">Name</option>
+                      <option value="dateTaken">Date Taken</option>
+                      <option value="location">Location</option>
+                      <option value="Photographer">Photographer</option>
+                    </select>
+                    <br><br>
+                    <input type="submit" value="Submit">
+                  </form>
+                   
+      </div>
+      </body>
+      </html>
+
 <?php
   // create short variable name
-  $document_root = $_SERVER['DOCUMENT_ROOT'];
-  $file_name = $_FILES["fileToUpload"]["name"];
+  //$document_root = $_SERVER['DOCUMENT_ROOT'];
+  //$file_name = $_FILES["fileToUpload"]["name"];
 ?>
 
 <!-- Store data from text-box to a file -->
@@ -18,14 +47,11 @@
 <!-- Sort strings from the file and output them to html -->
 
 <?php
-$path = "data.txt";
+/*$path = "data.txt";
 $photoname_ = trim($_POST['photoname_']);
 $email = trim($_POST['date_']);
 $name = trim($_POST['photographer_']);
-$location = trim($_POST['location_']);)
-
-function 
-
+$location = trim($_POST['location_']);*/
 
 
 ?> 
