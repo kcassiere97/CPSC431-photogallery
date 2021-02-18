7dfe800dfb82972037551fce9f3a35ab02cdd37a
kizar <kcassiere97@csu.fullerton.edu>
2021-02-17 19:07:03 -0800

    add

--- gallery.php
+++ gallery.php
@@ -1,7 +1,7 @@
 <?php
   // create short variable name
   $document_root = $_SERVER['DOCUMENT_ROOT'];
-  $file_name = $_FILES["fileToUpload"]["name"];
+  echo "Hello World";
 ?>
 
 <!-- Store data from text-box to a file -->
@@ -9,7 +9,7 @@
  $path = 'data.txt';
  if (isset($_POST['photoname_']) && isset($_POST['date_']) && isset($_POST['photographer_']) && isset($_POST['location_'])) {
     $fh = fopen($path,"a+");
-    $string = $_POST['photoname_'].' - '.$_POST['data_'].' - '.$_POST['photographer_'].' - '.$_POST['location_'];
+    $string = $_POST['photoname_'].' - '.$_POST['date_'].' - '.$_POST['photographer_'].' - '.$_POST['location_'];
     fwrite($fh,$string); // Write information to the file
     fclose($fh); // Close the file
  }
@@ -18,14 +18,21 @@
 <!-- Sort strings from the file and output them to html -->
 
 <?php
-$path = "data.txt";
-$photoname_ = trim($_POST['photoname_']);
-$email = trim($_POST['date_']);
-$name = trim($_POST['photographer_']);
-$location = trim($_POST['location_']);)
+    $path = "data.txt";
+    // $photoname_ = trim($_POST['photoname_']);
+    // $email = trim($_POST['date_']);
+    // $name = trim($_POST['photographer_']);
+    // $location = trim($_POST['location_']);
 
-function 
+    $strings = file("$document_root/data.txt")
+
+    // count the number of photos in the array
+    $number_of_photos = count($strings);
 
 
+    if ($number_of_photos == 0) {
+      echo "<p><strong>No photos.<br />
+            Please try again later.</strong></p>";
+    }
 
 ?> 
